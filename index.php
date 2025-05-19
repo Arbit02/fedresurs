00b982494e66ae2b53d2b8f44c5e38e3dd8695e8
Arbit02 <167581541+Arbit02@users.noreply.github.com>
2025-05-19 21:27:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     $filePath = 'fedresurs_cookies.json';
-
-    // Проверка существования файла
     if (file_exists($filePath)) {
         header('Content-Type: application/json');
         header('Content-Disposition: attachment; filename="fedresurs_cookies.json"');
@@ -12,14 +10,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         readfile($filePath);
         exit;
     } else {
-        // Файл не найден — отправим JSON с ошибкой
         http_response_code(404);
         echo json_encode(['error' => 'Файл не найден']);
         exit;
     }
 
 } else {
-    // Если не GET-запрос, вернем 405 Method Not Allowed
     http_response_code(405);
     header('Allow: GET');
     echo json_encode(['error' => 'Метод не поддерживается']);
